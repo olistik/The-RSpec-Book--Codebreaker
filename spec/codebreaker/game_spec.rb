3bca49c79e8cdb0defb751fae2886de2bb093b65
Maurizio De Magnis <maurizio.demagnis@gmail.com>
2012-04-17 15:49:47 +0200

    Completes chapter 6.

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -9,12 +9,12 @@ module Codebreaker
 
       it "sends a welcome message" do
         output.should_receive(:puts).with('Welcome to Codebreaker!')
-        game.start
+        game.start('1234')
       end
 
       it "prompts for the first guess" do
         output.should_receive(:puts).with('Enter guess:')
-        game.start
+        game.start('1234')
       end
     end
   end
